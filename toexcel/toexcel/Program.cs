0a4c0b2953e28e360bcbb40cf6f8fe01e9eda4be
matyas.kloub <matyas.kloub@seznam.cz>
2018-10-09 10:45:30 +0200

    not working v0.01

---
+++ toexcel/toexcel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+using System.Xml;
+using System.Drawing;
+using OfficeOpenXml.Style;
+
+namespace toexcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Spectyp person = new Spectyp();
+
+            person.Name = "Pepa";
+            person.Age = 26;
+
+
+            using (var package = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = package.Workbook.Add("Inventory");
+
+
+
+            }
+
+        }
+    }
+}
---
+++ toexcel/toexcel/Spectyp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toexcel
+{
+    class Spectyp
+    {
+        public string Name;
+        public int Age;
+
+    }
+}
