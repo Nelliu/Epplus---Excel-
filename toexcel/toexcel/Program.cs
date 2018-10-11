d26a9b6518834c7d74e03950b52409e32d752804
matyas.kloub <matyas.kloub@seznam.cz>
2018-10-11 16:06:12 +0200

    creating table

--- toexcel/toexcel/Program.cs
+++ toexcel/toexcel/Program.cs
@@ -8,6 +8,7 @@ using System.Xml;
 using System.Drawing;
 using OfficeOpenXml.Style;
 using System.IO;
+using OfficeOpenXml.Table;
 
 namespace toexcel
 {
@@ -40,9 +41,11 @@ namespace toexcel
             
             using (var package = new ExcelPackage())
             {
+                package.Workbook.Worksheets.Add("People");
+                package.Workbook.Worksheets.Add("In Tab");
 
-                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("People");
-                ExcelWorksheet tablew    = package.Workbook.Worksheets.Add("In Tab"); // table worksheet
+                var worksheet = package.Workbook.Worksheets["People"];
+                var tablew    = package.Workbook.Worksheets["In Tab"]; // table worksheet
 
                 using (var range = worksheet.Cells[1, 1, 1, 3])
                 {
@@ -65,15 +68,34 @@ namespace toexcel
                     peoplecs++;
                 }
                 worksheet.Column(2).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                //package.SaveAs(new FileInfo(@"D:\kloubma16\Epplus\toexcel\test.xlsx"));
+                
+
+                tablew.Cells[2,1].Value = 1;
+                tablew.Cells[3,1].Value = 1;
+                
+                var tableRange = tablew.Cells[1, 1, peopleC, 1];
+
+                var table = tablew.Tables.Add(tableRange, "table1");
+                table.ShowTotal = true;
+                table.TableStyle = TableStyles.Light2;
+
+                table.Columns[0].TotalsRowFormula = "=SUBTOTAL(102;[Column1])";
+                //table.Columns[2].TotalsRowFormula = "SUBTOTAL(109,[Column2])";
+                //table.Columns[3].TotalsRowFormula = "SUBTOTAL(101,[Column3])";
 
 
-                tablew.Cells["A1"].Value = "hello";
 
 
 
 
-                package.Save();
+
+
+
+
+
+
+                package.SaveAs(new FileInfo(@"D:\kloubma16\Epplus\toexcel\test.xlsx"));
+                //package.Save();
 
 
             }
